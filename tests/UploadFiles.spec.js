9d34c2873d4f60c9072020722ccbc7d2c9b01a97
Sai-Divya-Sree-NV <snagapud@cougarnet.uh.edu>
2025-04-11 15:27:18 -0500

    Initial commit with existing files

--- tests/UploadFiles.spec.js
+++ tests/UploadFiles.spec.js
@@ -1,9 +1,9 @@
-const{text,expect} = require('@playwright/test')
+const{test,expect} = require('@playwright/test')
 
 test('SIngle File',async({page})=>{
    
     await page.goto('https://www.careerbuilder.com/')
 
     await page.locator('//span[normalize-space()="Upload/Build Resume"]').click()
-    await page.locator('')
+   
 })
